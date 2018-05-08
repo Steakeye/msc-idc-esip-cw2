63bd6a7d93d3482b6cfd361eb81e9b57cee1b5d9
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-05-08 22:44:21 +0100

    wip: starting on a history function

---
+++ CustomAR/Assets/DYG/scripts/utils/History.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class History : MonoBehaviour {
+
+	private static List<string> history = new List<string>();
+	
+	// Use this for initialization
+	void Start ()
+	{
+		string currentSceneName = SceneManager.GetActiveScene().name;
+		history.Add("history-entry");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoBack()
+	{
+		
+	}
+}
