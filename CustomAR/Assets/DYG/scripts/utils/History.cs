14f257f33ed659fc8f5171f61cfef4a2cecf8c5d
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-05-09 12:32:13 +0100

    wip: adding a history

--- CustomAR/Assets/DYG/scripts/utils/History.cs
+++ CustomAR/Assets/DYG/scripts/utils/History.cs
@@ -6,13 +6,18 @@ using Vuforia;
 
 public class History : MonoBehaviour {
 
-	private static List<string> history = new List<string>();
+	private static Stack<string> history = new Stack<string>();
 	
 	// Use this for initialization
 	void Start ()
 	{
 		string currentSceneName = SceneManager.GetActiveScene().name;
-		history.Add("history-entry");
+
+		//Don'ty allow repeat entries into history
+		if (history.Count == 0 || history.Peek() != currentSceneName)
+		{
+			history.Push(currentSceneName);
+		}
 	}
 	
 	// Update is called once per frame
@@ -22,6 +27,11 @@ public class History : MonoBehaviour {
 
 	public void GoBack()
 	{
-		
+		//Let's make sure we cna actually go back tosomething
+		if (history.Count > 1) 
+		{
+			history.Pop();
+			SceneManager.LoadScene(history.Peek());
+		}
 	}
 }
